14cd73996264bd743b3a608b673cdefc807922f4
brunnapontual <brunnapontual@hotmail.com>
2026-05-11 23:47:27 -0300

    initial config fastAPI + backend db

---
+++ backend/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.database import init_db
+
+app = FastAPI(title="FaceTag API")
+
+
+@app.on_event("startup")
+def startup():
+    init_db()
+
+
+@app.get("/")
+def root():
+    return {"status": "ok", "message": "FaceTag backend funcionando"}
